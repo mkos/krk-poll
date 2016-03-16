5967641f1ff48dae86a6c93fd4e4f064ad1185a1
kosa <kosinski.michal@gmail.com>
2016-03-16 16:32:47 +0100

    update getters with error handling and cover them with unit tests; add comments.

--- test/test_config.py
+++ test/test_config.py
@@ -17,6 +17,38 @@ class TestConfig(unittest.TestCase):
         config = self.run_config({'config': {}})
         self.assertEqual({}, config.get_raw_config(), 'Config should be empty')
 
-    def test_get_raw_stations(self):
+    def test_get_raw_stations_no_stations_key(self):
+        config = self.run_config({'config': {}})
+        with self.assertRaises(wios.ResponseError):
+            config.get_raw_stations()
+
+    def test_get_raw_stations_sunny_day(self):
         config = self.run_config({'config': {'stations': []}})
         self.assertEqual([], config.get_raw_stations(), 'Config should be empty')
+
+    def test_get_raw_channels_no_channels_key(self):
+        config = self.run_config({'config': {}})
+        with self.assertRaises(wios.ResponseError):
+            config.get_raw_channels()
+
+    def test_get_raw_channels_sunny_day(self):
+        config = self.run_config({'config': {'channels': []}})
+        self.assertEqual([], config.get_raw_channels(), 'Config should be empty')
+
+    def test_get_raw_params_no_params_key(self):
+        config = self.run_config({'config': {}})
+        with self.assertRaises(wios.ResponseError):
+            config.get_raw_params()
+
+    def test_get_raw_params_sunny_day(self):
+        config = self.run_config({'config': {'params': []}})
+        self.assertEqual([], config.get_raw_params(), 'Config should be empty')
+
+    def test_get_raw_thresholds_no_thresholds_key(self):
+        config = self.run_config({'config': {}})
+        with self.assertRaises(wios.ResponseError):
+            config.get_raw_thresholds()
+
+    def test_get_raw_thresholds_sunny_day(self):
+        config = self.run_config({'config': {'thresholds': []}})
+        self.assertEqual([], config.get_raw_thresholds(), 'Config should be empty')
