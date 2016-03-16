1247ffbca481c6cb3f1678460a4bc835a46b9075
kosa <kosinski.michal@gmail.com>
2016-03-16 12:19:57 +0100

    add some test to existing functions

--- test/test_config.py
+++ test/test_config.py
@@ -4,10 +4,19 @@ import mock
 
 
 class TestConfig(unittest.TestCase):
-    def setUp(self):
 
-        wios.xml_http_request = mock.MagicMock(return_value={'config': {}})
-        self.config = wios.Config('someurl')
+    def run_config(self, return_value):
+        wios.xml_http_request = mock.MagicMock(return_value=return_value)
+        return wios.Config('someurl')
 
-    def test_config_get_raw_config(self):
-        self.assertEqual({}, self.config.get_raw_config())
+    def test_update_no_config_key(self):
+        with self.assertRaises(wios.ResponseError):
+            self.run_config({})
+
+    def test_get_raw_config_sunny_day(self):
+        config = self.run_config({'config': {}})
+        self.assertEqual({}, config.get_raw_config(), 'Config should be empty')
+
+    def test_get_raw_stations(self):
+        config = self.run_config({'config': {'stations': []}})
+        self.assertEqual([], config.get_raw_stations(), 'Config should be empty')
